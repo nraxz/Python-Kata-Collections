7efd910c00e3b1093da0e550186bdc717d80dfe5
- <thenraxz@gmail.com>
2022-04-05 19:41:58 +0100

    Created a Making chang Function

---
+++ Making Change/MakingChange.py
@@ -0,0 +1,10 @@
+changes = [50,25,10,5,1]
+
+def making_change(n):
+    for x in changes:
+        if(n >= x):
+            print(x,'s: ', int(n / x))
+            n = n % x
+        else:
+            print(x,'s: ', int(n / x))
+making_change(73)
